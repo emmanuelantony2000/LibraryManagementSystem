82d23f95d6aefc32774f092678e0daf9dbe1e1a7
emmanuelantony2000 <emmanuelantony2000@gmail.com>
2018-10-31 18:06:48 +0530

    changed inputBooks.py

--- inputBooks.py
+++ inputBooks.py
@@ -9,9 +9,16 @@ print(data)
 data.dropna(inplace = True)
 newline = scan()
 
+condition = (data[variables.columns[0]] == newline[0])[0]
 
-newline = pd.DataFrame(scan(), columns=variables.columns)
-data = data.append(newline, ignore_index=True)
+if condition:
+    data.loc[[variables.colums[0]]==newline[0], \
+    variables.colums[-1]] += 1
+else:
+    newline.append(1)
+    newline = pd.DataFrame(scan(), columns=variables.columns)
+    data = data.append(newline, ignore_index=True)
+
 #data = data.union(newline)
 #data.to_csv('books.csv')
 data.to_pickle('books.pickle')
